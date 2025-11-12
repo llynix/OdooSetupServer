110373de9e995bdc00820b3b03c20e14bb5a35de
Anthony Taylor <ataylor@fallsgeek.com>
2025-11-12 13:34:14 -0600

    Added unattended install -y flag

--- odoo_install.sh
+++ odoo_install.sh
@@ -77,7 +77,7 @@ echo -e "\n---- Update Server ----"
 # sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ xenial main"
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install libpq-dev
+sudo apt-get install libpq-dev -y
 
 #--------------------------------------------------
 # Install PostgreSQL Server
@@ -88,7 +88,7 @@ if [ $INSTALL_POSTGRESQL_FOURTEEN = "True" ]; then
     sudo curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
     sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
     sudo apt-get update
-    sudo apt-get install postgresql-16
+    sudo apt-get install postgresql-16 -y
 else
     echo -e "\n---- Installing the default postgreSQL version based on Linux version ----"
     sudo apt-get install postgresql postgresql-server-dev-all -y
