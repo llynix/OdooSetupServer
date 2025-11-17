772118b69899d414edee4305cf391caead49e96d
Anthony Taylor <ataylor@fallsgeek.com>
2025-11-17 15:52:39 -0600

    Openssh unattended

--- odoo_install.sh
+++ odoo_install.sh
@@ -76,7 +76,15 @@ echo -e "\n---- Update Server ----"
 # libpng12-0 dependency for wkhtmltopdf for older Ubuntu versions
 # sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ xenial main"
 sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y
+# For some reason openssh likes to prompt you to keep the old file.
+# Hopefully this does it automatically
+sudo DEBIAN_FRONTEND=noninteractive \
+UCF_FORCE_CONFFOLD=1 \
+UCF_FORCE_CONFDEF=1 \
+apt -o Dpkg::Options::="--force-confdef" \
+-o Dpkg::Options::="--force-confold" \
+install openssh-server -y
+sudo apt-get upgrade -y
 sudo apt-get install libpq-dev -y
 
 #--------------------------------------------------
